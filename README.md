cd8e4f7147851679886d0489011750bb72a2ff88
Mickael van der Beek <mickael@woorank.com>
2017-02-28 00:34:47 +0100

    Adding plots for all three types of shots for both environements

--- README.md
+++ README.md
@@ -16,3 +16,6 @@ Regular Season | Tourney
 :-------------------------:|:-------------------------:
 ![](./plots/season-score-by-location-plot.png) | ![](./plots/tourney-score-by-location-plot.png)
 ![](./plots/season-score-difference-plot.png) | ![](./plots/tourney-score-difference-plot.png)
+![](./plots/season-field-shot-ratio-plot.png) | ![](./plots/tourney-field-shot-ratio-plot.png)
+![](./plots/season-three-pointer-ratio-plot.png) | ![](./plots/tourney-three-pointer-ratio-plot.png)
+![](./plots/season-free-shot-ratio-plot.png) | ![](./plots/tourney-field-free-ration-plo.png)
---
+++ src/tourney-ratios-by-shot-plot.py
@@ -0,0 +1,79 @@
+from matplotlib import pyplot
+
+days = []
+fieldShots = []
+threeShots = []
+freeShots = []
+labels = []
+
+dates = set()
+colors = {
+	'W': 0.33,
+	'L': 0.66
+}
+
+with open('./data/TourneyDetailedResults.csv') as csv:
+	for (index, line) in enumerate(csv):
+		if index == 0:
+			continue
+
+		components = line.split(',')
+
+		date = components[0] + '-' + components[1]
+
+		if date not in dates:
+			dates.add(date)
+
+		day = len(dates)
+
+		wFieldShotsRatio = int(components[8], 10) / float(max(int(components[9], 10), 1))
+		wThreeShotsRatio = int(components[10], 10) / float(max(int(components[11], 10), 1))
+		wFreeShotsRatio = int(components[12], 10) / float(max(int(components[13], 10), 1))
+
+		lFieldShotsRatio = int(components[21], 10) / float(max(int(components[22], 10), 1))
+		lThreeShotsRatio = int(components[23], 10) / float(max(int(components[24], 10), 1))
+		lFreeShotsRatio = int(components[25], 10) / float(max(int(components[26], 10), 1))
+
+		days.append(day)
+		fieldShots.append(wFieldShotsRatio)
+		threeShots.append(wThreeShotsRatio)
+		freeShots.append(wFreeShotsRatio)
+		labels.append(colors['W'])
+
+		days.append(day)
+		fieldShots.append(lFieldShotsRatio)
+		threeShots.append(lThreeShotsRatio)
+		freeShots.append(lFreeShotsRatio)
+		labels.append(colors['L'])
+
+pyplot.scatter(days, fieldShots, s=1, c=labels, alpha=0.5)
+
+pyplot.xlabel('Day')
+pyplot.ylabel('Field Shot Ratio')
+
+pyplot.title('Tourney - Field Shot Ratio')
+
+pyplot.savefig('./plots/tourney-field-shot-ratio-plot.png', bbox_inches='tight')
+
+pyplot.clf()
+
+pyplot.scatter(days, threeShots, s=1, c=labels, alpha=0.5)
+
+pyplot.xlabel('Day')
+pyplot.ylabel('Three Pointer Ratio')
+
+pyplot.title('Tourney - Three Pointer Ratio')
+
+pyplot.savefig('./plots/tourney-three-pointer-ratio-plot.png', bbox_inches='tight')
+
+
+pyplot.clf()
+
+pyplot.scatter(days, freeShots, s=1, c=labels, alpha=0.5)
+
+pyplot.xlabel('Day')
+pyplot.ylabel('Free Shot Ratio')
+
+pyplot.title('Tourney - Free Shot Ratio')
+
+pyplot.savefig('./plots/tourney-free-shot-ratio-plot.png', bbox_inches='tight')
